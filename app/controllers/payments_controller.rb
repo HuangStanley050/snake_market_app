894309822f1d765729e444edff7ee663bdf57e08
Stanley Huang <huang.stanley050@gmail.com>
2019-10-24 15:49:15 +1100

    getting result back and showing more information on view for success purchage

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,7 +2,16 @@
 
 class PaymentsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:webhook]
-  def success; end
+  def success
+    userId = params['userId']
+    listingId = params['listingId']
+    @purchaser = User.find(userId).email
+    @item = Listing.find(listingId).title
+    @price = Listing.find(listingId).price
+    # puts "#{User.find(userId).email} purchased..."
+    # puts "The item is #{Listing.find(listingId).title}"
+    # puts "The price is #{Listing.find(listingId).price}"
+  end
 
   def webhook
     payment_id = params[:data][:object][:payment_intent]
@@ -12,7 +21,7 @@ class PaymentsController < ApplicationController
 
     p 'listing id ' + listing_id
     p 'user id ' + user_id
-
+    # details of what you bough from the returned params
     status 200
   end
 end
