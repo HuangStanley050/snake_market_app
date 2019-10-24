b29101f35db7e237c0f33e1af8b75c900a183d25
Stanley Huang <huang.stanley050@gmail.com>
2019-10-24 12:18:52 +1100

    make payment to Stripe API and redirect back after success

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   patch '/listings/:id', to: 'listings#update'
   delete '/listings/:id', to: 'listings#destroy'
   get '/listings/:id/edit', to: 'listings#edit', as: 'edit_listing'
-
+  get '/payments/success', to: 'payments#success'
   get '/breeds/new', to: 'breeds#new'
   get '/breeds/:id', to: 'breeds#show'
   get '/breeds', to: 'breeds#index'
